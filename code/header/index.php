d9c07c7977922f0843885e81e9b9d373299f0aaa
VietPhong280120 <1851061519@e.tlu.edu.vn>
2021-05-19 14:43:34 +0700

    Nguyen viet phong - creat header

---
+++ code/header/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moon House</title>
+    <!--Google font-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap">
+    <!--Google font awesome-->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <!--CSS-->
+    <link rel="stylesheet" href="./asset/style.css">
+</head>
+<body>
+    <!--header-->
+    <div class="header">
+        <div class="container">
+            <div class="header__menu">
+                <div class="header__menu-logo">
+                   <a href="index.php" class="header__menu-link">Moon House</a>
+                </div>
+                <div class="header__menu-search">
+                    <form action="">
+                    <input class="form-control mr-sm-2 search" type="text" placeholder="  Bạn cần mua gì hôm nay ?"autocomplete="off">
+                    <button aria-label= "btn-search">
+                    <i class="fas fa-search"></i>
+                    </button>
+                    </form>
+                </div>
+                <ul class="header__menu-list">
+                    <li class="list__item"> 
+                        <a href="#" class="list__item-link">
+                        <i class="fas fa-shopping-cart" ></i>
+                            Giỏ Hàng</a>
+                    </li>
+                    <li class="list__item">
+                        <a href="#" class="list__item-link ">
+                        <i class="far fa-user-circle"></i>
+                            Tài khoản</a>
+                    </li>
+                    
+                </ul>
+            </div>
+
+        </div>
+    </div>
+    <!--end header-->
+</body>
+</html>
